4eeed752d00ecd87c139f656e027d03e226f6e95
ahmed gad <38489010+gad1231@users.noreply.github.com>
2020-03-02 16:46:30 +0200

    Update json.h
    remove unnecessary comments

--- json.h
+++ json.h
@@ -3,9 +3,6 @@
  Creation Date:	20SEP2018
  Functionality:  to format data in json format
 
- Note:-
- till now the current functions is to deal with terminal from different modules - so the current file considered to be
- the one which is the next upper layer to the Terminal files
 
  */
 #ifndef __JSON_H__
